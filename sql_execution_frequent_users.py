f0c623db27a6fb4d20b70e4cf562526ce46bb8ad
amitrani6 <amitrani1206@gmail.com>
2019-08-07 01:44:17 -0400

    Tested the notebook and python script functionality, approved

--- sql_execution_frequent_users.py
+++ sql_execution_frequent_users.py
@@ -1,10 +1,11 @@
 # This python file opens and executes the SQL commands listed in 'sqlite_commands.sql'
 # See 'SQLite_Query.ipynb' for a detailed breakdown of the logic behind the commands
 
-# Imports the SQLite3 library, connects to the 'testdb.db' database, and creates
+# Imports the necessary libraries, connects to the 'testdb.db' database, and creates
 # a cursor
 import sqlite3 
-connection = sqlite3.connect('testdb.db')
+import pandas as pd
+connection = sqlite3.connect(r'ProgrammerTest/testdb.db')
 cursor = connection.cursor()
 
 # Opens the .sql file with the main SQL command for entering the most frequent users
@@ -15,3 +16,15 @@ cursor.executescript(sql) # Runs the SQL command
 sql_file.close() # Closes the SQL file
 
 connection.commit() # Commits the changes to the 'frequent_browsers' table
+
+# This function takes in a SQL query and returns the result
+# as a pandas dataframe
+def create_pandas_table(sql_query, database = connection):
+    table = pd.read_sql_query(sql_query, database)
+    return table
+
+print('\nTable: frequent_browsers\n')
+
+print(create_pandas_table('''SELECT * from frequent_browsers;'''))
+
+print("\n")
